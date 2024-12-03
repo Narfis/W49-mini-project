256347a6fc46b7aed6a5ad8795c15dbfe90e1939
Narfis <Sebban.hansson@gmail.com>
2024-12-03 11:08:10 +0100

    Add currency handling and asset management features
    - Added `Currency` class in `LiveCurrency.cs` to define currency codes and store exchange rates.
    - Introduced `LiveCurrency` class to fetch exchange rates from an XML document and handle currency conversion.
    - Modified `Program.cs` to remove default message and add `FillTracker` method for populating `AssetManager` with `Smartphone` and `Computer` objects.
    - Updated `main` function to create `AssetManager` instance, populate it with assets, and fetch exchange rates using `LiveCurrency.FetchRates`.

---
+++ LiveCurrency.cs
@@ -0,0 +1,77 @@
+﻿using System.Xml;
+
+
+class Currency
+{
+    public static string USD = "USD";
+    public static string SEK = "SEK";
+    public static string EUR = "EUR";
+    private string CurrencyCode { get; set; }
+    private double ExchangeRateFromEUR { get; set; }
+    public Currency(string currencyCode, double exchangeRateFromEUR)
+    {
+        CurrencyCode = currencyCode;
+        ExchangeRateFromEUR = exchangeRateFromEUR;
+    }
+
+    public string GetCurrencyCode()
+    {
+        return CurrencyCode;
+    }
+
+    public double GetExchangeRateFromEUR()
+    {
+        return ExchangeRateFromEUR;
+    }
+}
+
+
+    public class LiveCurrency // Class that handles fetching the exchange rates and converting currencies
+{
+    private static List<Currency> currencyList = new List<Currency>();
+
+
+    public static void FetchRates()
+    {
+        string url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"; // Exchange rate XML document
+
+        XmlTextReader reader = new XmlTextReader(url);
+        while (reader.Read()) // Goes through the XML document and saves the currency exchange rates to the local list
+        {
+            if (reader.NodeType == XmlNodeType.Element)
+            {
+                while (reader.MoveToNextAttribute()) 
+                {
+                    if (reader.Name == "currency") // Identifies each currency attribute and saves the currency code and rate as an object
+                    {
+                        string currencyCode = reader.Value;
+                        reader.MoveToNextAttribute();
+                        double rate = double.Parse(reader.Value.Replace(".", ","));
+                        currencyList.Add(new Currency(currencyCode, rate));
+                    }
+                }
+            }
+        }
+    }
+
+    public static double Convert(double input, string fromCurrency, string toCurrency) // Method that uses the fetched rates to convert between the given rates via Euro
+    {
+        double value = 0;
+            
+        if (fromCurrency == "EUR")
+        {
+            value = input * currencyList.Find(c => c.GetCurrencyCode() == toCurrency).GetExchangeRateFromEUR();
+        }
+        else if (toCurrency == "EUR")
+        {
+            value = input / currencyList.Find(c => c.GetCurrencyCode() == fromCurrency).GetExchangeRateFromEUR();
+        }
+        else
+        {
+            value = input / currencyList.Find(c => c.GetCurrencyCode()== fromCurrency).GetExchangeRateFromEUR();
+            value *= currencyList.Find(c => c.GetCurrencyCode() == toCurrency).GetExchangeRateFromEUR();
+        }
+
+        return value;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+void FillTracker(AssetManager tracker)
+{
+    tracker.AddAsset(new Smartphone(new Price(200, Currency.USD), DateTime.Now.AddMonths(-36 + 4), "Motorola", "X3", Countries.USA));
+    tracker.AddAsset(new Smartphone(new Price(400, Currency.USD), DateTime.Now.AddMonths(-36 + 5), "Motorola", "X3", Countries.USA));
+    tracker.AddAsset(new Smartphone(new Price(400, Currency.USD), DateTime.Now.AddMonths(-36 + 10), "Motorola", "X2", Countries.USA));
+    tracker.AddAsset(new Smartphone(new Price(4500, Currency.SEK), DateTime.Now.AddMonths(-36 + 6), "Samsung", "Galaxy 10", Countries.Sweden));
+    tracker.AddAsset(new Smartphone(new Price(4500, Currency.SEK), DateTime.Now.AddMonths(-36 + 7), "Samsung", "Galaxy 10", Countries.Sweden));
+    tracker.AddAsset(new Smartphone(new Price(3000, Currency.SEK), DateTime.Now.AddMonths(-36 + 4), "Sony", "XPeria 7", Countries.Sweden));
+    tracker.AddAsset(new Smartphone(new Price(3000, Currency.SEK), DateTime.Now.AddMonths(-36 + 5), "Sony", "XPeria 7", Countries.Sweden));
+    tracker.AddAsset(new Smartphone(new Price(220, Currency.EUR), DateTime.Now.AddMonths(-36 + 12), "Siemens", "Brick", Countries.Germany));
+    tracker.AddAsset(new Computer(new Price(100, Currency.USD), DateTime.Now.AddMonths(-38), "Dell", "Desktop 900", Countries.USA));
+    tracker.AddAsset(new Computer(new Price(100, Currency.USD), DateTime.Now.AddMonths(-37), "Dell", "Desktop 900", Countries.USA));
+    tracker.AddAsset(new Computer(new Price(300, Currency.USD), DateTime.Now.AddMonths(-36 + 1), "Lenovo", "X100", Countries.USA));
+    tracker.AddAsset(new Computer(new Price(300, Currency.USD), DateTime.Now.AddMonths(-36 + 4), "Lenovo", "X200", Countries.USA));
+    tracker.AddAsset(new Computer(new Price(500, Currency.USD), DateTime.Now.AddMonths(-36 + 9), "Lenovo", "X300", Countries.USA));
+    tracker.AddAsset(new Computer(new Price(1500, Currency.SEK), DateTime.Now.AddMonths(-36 + 7), "Dell", "Optiplex 100", Countries.Sweden));
+    tracker.AddAsset(new Computer(new Price(1400, Currency.SEK), DateTime.Now.AddMonths(-36 + 8), "Dell", "Optiplex 200", Countries.Sweden));
+    tracker.AddAsset(new Computer(new Price(1300, Currency.SEK), DateTime.Now.AddMonths(-36 + 9), "Dell", "Optiplex 300", Countries.Sweden));
+    tracker.AddAsset(new Computer(new Price(1600, Currency.EUR), DateTime.Now.AddMonths(-36 + 14), "Asus", "ROG 600", Countries.Germany));
+    tracker.AddAsset(new Computer(new Price(1200, Currency.EUR), DateTime.Now.AddMonths(-36 + 4), "Asus", "ROG 500", Countries.Germany));
+    tracker.AddAsset(new Computer(new Price(1200, Currency.EUR), DateTime.Now.AddMonths(-36 + 3), "Asus", "ROG 500", Countries.Germany));
+    tracker.AddAsset(new Computer(new Price(1300, Currency.EUR), DateTime.Now.AddMonths(-36 + 2), "Asus", "ROG 500", Countries.Germany));
+}
+
+
+int main()
+{
+    AssetManager tracker = new AssetManager();
+
+    FillTracker(tracker);
+    LiveCurrency.FetchRates();
+
+    return 0;
+}
+
+main();
