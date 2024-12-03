acd64b85c397f5beb1dc80390e8f07bfc072792d
Narfis <Sebban.hansson@gmail.com>
2024-12-03 11:07:38 +0100

    Add Asset management system with various asset types
    Introduced a `Countries` enum with values `USA`, `Sweden`, and `Germany`. Added a `Price` class with `Value` and `Currency` properties. Created an `Asset` class with properties `Price`, `PurchaseDate`, `Manufacturer`, `Model`, and `Country`. Added `Smartphone` and `Computer` classes inheriting from `Asset`. Implemented an `AssetManager` class with a private list of `Asset` objects and an `AddAsset` method.

---
+++ Assets.cs
@@ -0,0 +1,59 @@
+﻿
+public enum Countries
+{
+    USA,
+    Sweden,
+    Germany
+}
+
+class Price
+{
+    private double Value { get; set; }
+    private string Currency { get; set; }
+    public Price(double value, string currency)
+    {
+        Value = value;
+        Currency = currency;
+    }
+}
+class Asset
+{
+    protected Price? Price { get; set; }
+    protected DateTime PurchaseDate { get; set; }
+    protected string? Manufacturer { get; set; }
+    protected string? Model { get; set; }
+    protected Countries Country { get; set; }
+}
+
+class Smartphone : Asset
+{
+    public Smartphone(Price price, DateTime purchaseDate, string manufacturer, string model, Countries country)
+    {
+        Price = price;
+        PurchaseDate = purchaseDate;
+        Manufacturer = manufacturer;
+        Model = model;
+        Country = country;
+    }
+}
+
+class Computer : Asset
+{
+    public Computer(Price price, DateTime purchaseDate, string manufacturer, string model, Countries country)
+    {
+        Price = price;
+        PurchaseDate = purchaseDate;
+        Manufacturer = manufacturer;
+        Model = model;
+        Country = country;
+    }
+}
+
+class AssetManager
+{
+    private List<Asset> AssetList = new List<Asset>();
+    public void AddAsset(Asset asset)
+    {
+        AssetList.Add(asset);
+    }
+}
